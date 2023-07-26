94926205b0578aa8293dfbdb5788acd70bd85119
JordanNitta <Jordannittarodrigues@gmail.com>
2023-07-25 18:02:59 -0700

    Energy page finished and fully responsive

--- client/src/components/SolarRoofComponent/ThirdPage.jsx
+++ client/src/components/SolarRoofComponent/ThirdPage.jsx
@@ -39,16 +39,28 @@ const ThirdPage = () => {
             {/*Large Devices */}
             <div className='hidden lg:flex'>
                 <div className='hidden lg:flex lg:flex-col lg:space-x-20 w-[30%] lg:pt-[45px] lg:px-[25px]'>
-                    <div className='hidden lg:flex lg:flex-col items-start'>
+                    <div className='hidden lg:flex lg:flex-col items-start lg:h-[95%]'>
                         <p className='text-[17px] text-main'>Energy Storage</p>
                         <p className='text-[22px] text-main xl:text-[28px]'>Protection Against<br></br>Outages</p>
                         <p className='text-[14px] text-primary'>Powerwall is a compact home battery that is bundled with Solar Roof, providing you with 24/7 energy security. It stores the energy you produce with Solar Roof so you can power your home anytime—at night or during an outage.</p>
+                        <div className='mt-auto flex flex-col items-center justify-center space-y-5 w-full'>
+                            <Link to='/'>
+                                <button className='border-[3px] rounded border-black  w-[350px]   h-10 text-[14px] text-main font-bold'>
+                                    Order Now
+                                </button>
+                            </Link>
+                            <Link to='/'>
+                                <button className='rounded bg-[#F4F4F4] w-[350px]  h-10 text-[14px] text-main font-bold'>
+                                    Learn More
+                                </button>
+                            </Link>
+                        </div>
                     </div>
                 </div>
-                <div className='w-[90%]'>
+                <div className='w-[85%]'>
                     <img src='https://digitalassets.tesla.com/tesla-contents/image/upload/h_1800,w_2880,c_fit,f_auto,q_auto:best/Solar-Roof-Energy-Desktop-Global' alt="solar roof image"
                         style={{
-                         // Set a larger height for small screens to zoom in
+                            // Set a larger height for small screens to zoom in
                             objectFit: 'cover', // To maintain aspect ratio and fill the container
                         }}
                         className=' w-full h-screen' />
