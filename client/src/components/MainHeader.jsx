5a1da1479c3b75abf104314a601982951ba51cb7
chhuonso <103080113+chhuonso@users.noreply.github.com>
2023-07-27 19:14:53 -0700

    deleted commented out lines

--- client/src/components/MainHeader.jsx
+++ client/src/components/MainHeader.jsx
@@ -29,35 +29,8 @@ const MainHeader = () => {
                         )
                     })
                 }
-                <div className='h-screen bg-cover bg-center flex flex-col justify-between snap-start' style={{ backgroundImage: `url(${Accessories2})` }} >
-                    <div className='relative text-center top-[15%]'>
-                        <h1 className='text-4xl font-bold '>Accessories</h1>
-                    </div>
-                    <div className='flex flex-col items-center gap-5 justify-center text-sm lg:flex-row md:flex-row relative'>
-                        <button className='bg-white rounded-md w-96 md:w-64 lg:mx-4 mt-2 h-10 text-[#393c41] p-2 relative top-[30%]'>Shop Now</button>
-                    </div>
-                </div>
             </div>
         </>
-        // <div className='h-screen bg-cover bg-center'>
-        //   <Navbar/>
-        //   <div >
-        //     <div className='absolute inset-x-0 top-[15%] text-center'>
-        //       <h1 className='text-4xl font-bold '>Model 3</h1>
-        //       <p className='underline underline-offset-4 pt-2 text-sm font-light'>Starting at $32,740</p>
-        //       <p className='text-sm'>After Federal Tax Credit</p>
-        //     </div>
-        //     <div>
-        //     </div>
-        //     <div className='flex flex-col items-center justify-center text-sm lg:flex-row absolute inset-x-0 bottom-[7%] '>
-        //       <button className='bg-[#393c41] rounded-md w-96 lg:w-64 lg:mx-4 mt-2 h-10  text-white p-2'>Order Now</button>
-        //       <button className='bg-white rounded-md w-96 lg:w-64 lg:mx-4 mt-4 h-10 text-[#393c41] p-2 '>Demo Drive</button>
-        //     </div>
-        //     <div className='absolute bottom-[2%] bg-[#393c41] w-12 h-10 rounded text-white flex justify-center items-center text-xl inset-x-12 lg:inset-y-100 lg:inset-x-[95%] animate-bounce'>
-        //       <BsChatLeftDots size={24} />
-        //     </div>
-        //   </div>
-        // </div>
     )
 }
 
