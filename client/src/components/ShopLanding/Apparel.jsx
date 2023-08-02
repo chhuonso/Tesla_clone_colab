b134f452b59192d1f300c9483ebd040ad871417e
JordanNitta <Jordannittarodrigues@gmail.com>
2023-08-01 17:22:24 -0700

    Working on the apparel page to make it responsive

--- client/src/components/ShopLanding/Apparel.jsx
+++ client/src/components/ShopLanding/Apparel.jsx
@@ -1,45 +1,74 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import MensMobile from '../../images/shopImages/men_mobile.avif'
+import MensDesktop from '../../images/shopImages/men_web.avif'
+
 import WomanMobile from '../../images/shopImages/women_mobile.avif'
+import WomanDesktop from '../../images/shopImages/women_web.avif'
+
 import KidMobile from '../../images/shopImages/Kids_mobile.avif'
+import KidDesktop from '../../images/shopImages/kids_web.avif'
+
 import PrimaryButton from '../btn/LargePrimaryBtn'
-import Wrapper from '../wrappers/ScrollSnap'
+
 const Apparel = () => {
+    const [,setBgImage] = useState(null)
+
     const apparelData = [
         {
             id: 1,
-            bgImg: `${MensMobile}`,
+            mobileImg: MensMobile,
+            desktopImg: MensDesktop,
             title: "Men's Apparel",
             path: "/category/apparel/men",
             btn: "Shop Now",
         },
         {
             id: 2,
-            bgImg: `${WomanMobile}`,
+            mobileImg: WomanMobile,
+            desktopImg: WomanDesktop,
             title: "Men's Apparel",
             path: "/category/apparel/woman",
             btn: "Shop Now",
         },
         {
             id: 3,
-            bgImg: `${KidMobile}`,
+            mobileImg: KidMobile,
+            desktopImg: KidDesktop,
             title: "Kid's Apparel",
             path: "/category/apparel/kid",
             btn: "Shop Now",
         }
     ]
+
+    // const imageChange = (mobileImg, desktopImg) => {
+    //     return window.innerWidth <= 600 ? mobileImg : desktopImg
+    // }
+
+    // useEffect(() => {
+    //     const handleResize = () => {
+    //         const newBgImage = imageChange(apparelData[0].mobileImg, apparelData[0].desktopImg)
+    //         setBgImage(newBgImage)
+    //     }
+    //     // Add a event which is resize that takes a callback function which will handle the resize
+    //     window.addEventListener('resize', handleResize)
+
+    //     // Clean up function to unmount 
+    //     return () => {
+    //         window.removeEventListener('resize', handleResize)
+    //     }
+    // }, [])
     return (
         <>
-            <Wrapper>
+            <div>
                 {
-                    apparelData.map(({ title, price, subTitle, bgImg, btn }, id) => {
+                    apparelData.map(({ title, mobileImg, desktopImg, btn }, id) => {
                         return (
-                            <div key={id} style={{ backgroundImage: `url(${bgImg})` }} className='h-screen bg-cover bg-center flex flex-col justify-between snap-start'>
+                            <div key={id} style={{ backgroundImage: `url(${mobileImg})` }} className='h-screen bg-cover bg-center flex flex-col justify-between snap-start'>
                                 <div className='relative text-center'>
-                                    <p className='underline underline-offset-4 pt-2 text-sm font-light'>{price}</p>
-                                    <p className='text-sm'>{subTitle}</p>
+                                    {/* <p className='underline underline-offset-4 pt-2 text-sm font-light'>{price}</p>
+                                    <p className='text-sm'>{subTitle}</p> */}
                                 </div>
                                 <div className='flex flex-col items-center gap-5 justify-end text-sm lg:flex-row md:flex-row relative mb-10'>
                                     <h1 className='text-4xl text-center font-bold w-96 md:w-64 lg:mx-4 mt-2 h-10 text-black top-[20%] p-2 text-[22px]'>{title}</h1>
@@ -49,7 +78,7 @@ const Apparel = () => {
                         )
                     })
                 }
-            </Wrapper>
+            </div>
         </>
     )
 }
