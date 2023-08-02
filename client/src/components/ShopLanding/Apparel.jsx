09ac74912dada0bf34d8fa7cc49730eab99baa1f
JordanNitta <Jordannittarodrigues@gmail.com>
2023-08-01 20:37:54 -0700

    Finish mobile design for the apparel page

--- client/src/components/ShopLanding/Apparel.jsx
+++ client/src/components/ShopLanding/Apparel.jsx
@@ -42,39 +42,70 @@ const Apparel = () => {
         }
     ]
 
-    // const imageChange = (mobileImg, desktopImg) => {
-    //     return window.innerWidth <= 600 ? mobileImg : desktopImg
-    // }
+    const imageChange = (mobileImg, desktopImg) => {
+        return window.innerWidth <= 600 ? mobileImg : desktopImg
+    }
 
-    // useEffect(() => {
-    //     const handleResize = () => {
-    //         const newBgImage = imageChange(apparelData[0].mobileImg, apparelData[0].desktopImg)
-    //         setBgImage(newBgImage)
-    //     }
-    //     // Add a event which is resize that takes a callback function which will handle the resize
-    //     window.addEventListener('resize', handleResize)
+    useEffect(() => {
+        const handleResize = () => {
+            const newBgImage = imageChange(apparelData[0].mobileImg, apparelData[0].desktopImg)
+            setBgImage(newBgImage)
+        }
+        // Add a event which is resize that takes a callback function which will handle the resize
+        window.addEventListener('resize', handleResize)
 
-    //     // Clean up function to unmount 
-    //     return () => {
-    //         window.removeEventListener('resize', handleResize)
-    //     }
-    // }, [])
+        // Clean up function to unmount 
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+
+    }, [])
     return (
         <>
-            <div>
+            <div className='sm:hidden'>
                 {
-                    apparelData.map(({ title, mobileImg, desktopImg, btn }, id) => {
+                    apparelData.map(({ title, mobileImg, btn, path }, id) => {
                         return (
                             <div key={id} style={{ backgroundImage: `url(${mobileImg})` }} className='h-screen bg-cover bg-center flex flex-col justify-between snap-start'>
                                 <div className='relative text-center'>
                                     {/* <p className='underline underline-offset-4 pt-2 text-sm font-light'>{price}</p>
                                     <p className='text-sm'>{subTitle}</p> */}
                                 </div>
-                                <div className='flex flex-col items-center gap-5 justify-end text-sm lg:flex-row md:flex-row relative mb-10'>
-                                    <h1 className='text-4xl text-center font-bold w-96 md:w-64 lg:mx-4 mt-2 h-10 text-black top-[20%] p-2 text-[22px]'>{title}</h1>
-                                    <PrimaryButton>{btn}</PrimaryButton>
+                                <div className='flex flex-col items-center gap-5 justify-end text-sm relative mb-10'>
+                                    <h1 className='text-4xl text-center font-bold w-96 mt-2 h-10 text-black top-[20%] p-2 text-[22px]'>{title}</h1>
+                                    <PrimaryButton>
+                                        <Link to={path}>{btn}</Link>
+                                        {btn}
+                                    </PrimaryButton>
                                 </div>
                             </div>
+                        )
+                    })
+                }
+            </div>
+            <div className='hidden sm:grid grid-cols-3 gap-5'>
+                {
+                    apparelData.map(({ title, desktopImg, btn, path }, id) => {
+                        return (
+                        <div key={id} style={{ 
+                            backgroundImage: `url(${desktopImg})`,
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                            backgroundRepeat: 'no-repeat', 
+                            }} className='h-screen  flex flex-col justify-between snap-start'>
+                            <div className='relative text-center'>
+                                {/* <p className='underline underline-offset-4 pt-2 text-sm font-light'>{price}</p>
+                                <p className='text-sm'>{subTitle}</p> */}
+                            </div>
+                            <div className='flex flex-col items-center gap-5 justify-end text-sm relative mb-10'>
+                                <h1 className='text-4xl text-center font-bold w-96 mt-2 h-10 text-black top-[20%] p-2 text-[22px]'>{title}</h1>
+                                <PrimaryButton>
+                                    <Link to={path}>{btn}
+                                        {btn}
+                                    </Link>
+                                </PrimaryButton>
+                            </div>
+                        </div>
                         )
                     })
                 }
