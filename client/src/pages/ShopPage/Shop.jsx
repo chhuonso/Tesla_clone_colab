b2b5ea5a040aee719a982e13e45e8ca4451385bd
JordanNitta <Jordannittarodrigues@gmail.com>
2023-08-01 17:22:57 -0700

    Added more images for the shop page

--- client/src/pages/ShopPage/Shop.jsx
+++ client/src/pages/ShopPage/Shop.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import ShopNavbar from '../../components/ShopNavbar/ShopNavbar'
 import CarouselShop from '../../components/ShopLanding/CarouselShop'
 import Apparel from '../../components/ShopLanding/Apparel'
+import CarAccessories from '../../components/ShopLanding/CarAccessories'
+import Wrapper from '../../components/wrappers/ScrollSnap'
 
 const ShopLanding = () => {
     return (
         <>
             <ShopNavbar />
-            <div>
+            <Wrapper>
                 <CarouselShop />
+                <CarAccessories />
                 <Apparel />
-            </div>
+            </Wrapper>
         </>
     )
 }
